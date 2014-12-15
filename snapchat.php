6fc36ee0a9b22087b8a95e21fed117feec36fe99
sallaben <steve@dot.net>
2014-12-15 17:14:07 -0500

    v1.1.1
    oops

--- snapchat.php
+++ snapchat.php
@@ -1,8 +1,8 @@
 <?php
 require_once("src/snapchat.php");
 
-$username = 'SNAPCHAT_USERNAME';
-$password = 'SNAPCHAT_PASSWORD';
+$username = 'entstorydeleter';
+$password = 'entjJAPLQ$RQ*@2ent';
 $snapchat = new Snapchat($username, $password); //create new instance of class Snapchat
 $snaps = $snapchat->getSnaps(); //get feed
 $snaps = json_decode(json_encode($snaps), true); //turn into php array
@@ -16,13 +16,13 @@ foreach($snaps as $item) {
             $data = $snapchat->getMedia($item['id']); //get received snap
             file_put_contents('media/temp.jpg', $data); //create temp file with the received snap
             $id = $snapchat->upload(Snapchat::MEDIA_IMAGE, file_get_contents('media/temp.jpg')); //upload the temp to story
-            $snapchat->setStory($id, Snapchat::MEDIA_IMAGE, 3); //set story 3 seconds long
+            $snapchat->setStory($id, Snapchat::MEDIA_IMAGE, 5); //set story 3 seconds long
             unlink("media/temp.jpg"); //delete temp
         } elseif($item['media_type'] == 1) { //if moving video
             $data = $snapchat->getMedia($item['id']); //get received snap
             file_put_contents('media/temp.mov', $data); //create temp file with the received sna
             $id = $snapchat->upload(Snapchat::MEDIA_VIDEO, file_get_contents('media/temp.mov')); //upload the temp to story
-            $snapchat->setStory($id, Snapchat::MEDIA_VIDEO); //set story
+            $snapchat->setStory($id, Snapchat::MEDIA_VIDEO, 5); //set story
             unlink("media/temp.mov"); //delete temp
         }
         $snapchat->markSnapViewed($item['id']); //mark as viewed, just in case
@@ -34,3 +34,5 @@ foreach($snaps as $item) {
 }
 $snapchat->clearFeed(); //clear feed (will become VERY long VERY soon and break program)
 ?>
+
+
